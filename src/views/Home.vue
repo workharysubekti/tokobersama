26075399216800cdb34aac34e02c0661d64c350c
Hary Subekti <work.harysubekti@gmail.com>
2026-04-26 14:48:02 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,7 +44,7 @@ const fetchProducts = async () => {
   try {
     const { data: prodData } = await supabase
       .from("products")
-      .select(`*, profiles!owner_id (username, full_name, avatar_url, )`)
+      .select(`*, profiles!owner_id (username, full_name, avatar_url)`)
       .eq("status", "active")
       .order("created_at", { ascending: false });
 
